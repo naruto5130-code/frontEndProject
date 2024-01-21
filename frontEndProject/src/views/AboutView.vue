9746f7cdabc9a92a8f80f05b60607c8be4be42ed
溫永靖 <naruto5130@gmail.com>
2024-01-21 12:54:45 +0800

    Initial commit

---
+++ frontEndProject/src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="getData">獲取資料</button>
+    <div v-if="responseData">
+      <p>{{ responseData }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      responseData: null,
+    };
+  },
+  methods: {
+    getData() {
+      // 發送 GET 請求
+      axios.get('http://localhost:8080/api/hello')
+        .then(response => {
+          // 處理 API 回應
+          this.responseData = response.data;
+        })
+        .catch(error => {
+          // 處理錯誤
+          console.error('Error fetching data:', error);
+        });
+      print(responseData);
+    },
+  },
+};
+</script>
